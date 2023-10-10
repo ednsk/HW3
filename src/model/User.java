190dbab1a85a63d3a9deeee5167ae78aa571fb5e
ednsk <119007272+ednsk@users.noreply.github.com>
2023-10-11 00:33:19 +0700

    Update user.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,1 +1,54 @@
+package model;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class User {
+    private String lastName;
+    private String firstName;
+    private String Surname;
+    private String phoneNumber;
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) throws IllegalArgumentException {
+        if (phoneNumber.matches("^((8|7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$")) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new IllegalArgumentException("Invalid phone number format.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return lastName + " " + firstName + " " + Surname + " " + phoneNumber;
+    }
+}
