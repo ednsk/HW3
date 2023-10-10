30539419c4dc19009aee95e2659b925bc1b90d1c
ednsk <119007272+ednsk@users.noreply.github.com>
2023-10-11 00:32:46 +0700

    Add files via upload

--- src/view/View.java
+++ src/view/View.java
@@ -1,1 +1,19 @@
+package view;
 
+import java.util.Scanner;
+
+public class View {
+    public String CreateUser(){
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter user data in the following format: Surname Name Patronymic PhoneNumber (10) : ");
+        return inp.nextLine();
+    }
+
+    public void Exception(Exception e){
+        Exception(e.getMessage());
+    }
+
+    public void Exception(String str){
+        System.out.println(str);
+    }
+}
